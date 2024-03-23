37c6bd8fc8c743994c9e1ceac269cb3f731fe447
guokaide <kaideguo@gmail.com>
2024-03-23 19:12:38 +0800

    refactor ConsumerApplication

--- kairpc-demo-consumer/src/main/java/com/kai/kairpc/demo/consumer/KairpcDemoConsumerApplication.java
+++ kairpc-demo-consumer/src/main/java/com/kai/kairpc/demo/consumer/KairpcDemoConsumerApplication.java
@@ -43,84 +43,86 @@ public class KairpcDemoConsumerApplication {
 
     @Bean
     public ApplicationRunner applicationRunner() {
-        return x -> {
-            // case: int -> User
-            System.out.println("case 1: ==> [常规 int 类型，返回 User 对象]");
-            User user = userService.findById(1);
-            System.out.println("userService.findById(1) = " + user);
-            System.out.println();
+        return x -> testAll();
+    }
 
-            // case: override
-            System.out.println("case 2: ==> [测试方法重载，同名方法，参数不同]");
-            User userKai = userService.findById(1, "kai");
-            System.out.println("userService.findById(1, \"kai\") = " + userKai);
-            System.out.println();
+    private void testAll() {
+        // case: int -> User
+        System.out.println("case 1: ==> [常规 int 类型，返回 User 对象]");
+        User user = userService.findById(1);
+        System.out.println("userService.findById(1) = " + user);
+        System.out.println();
 
-            // case: void -> String
-            System.out.println("case 3: ==> [测试返回字符串]");
-            System.out.println("userService.getName() = " + userService.getName());
-            System.out.println();
+        // case: override
+        System.out.println("case 2: ==> [测试方法重载，同名方法，参数不同]");
+        User userKai = userService.findById(1, "kai");
+        System.out.println("userService.findById(1, \"kai\") = " + userKai);
+        System.out.println();
 
-            // case: override
-            System.out.println("case 4: ==> [测试重载方法返回字符串]");
-            System.out.println("userService.getName(123) = " + userService.getName(123));
-            System.out.println();
+        // case: void -> String
+        System.out.println("case 3: ==> [测试返回字符串]");
+        System.out.println("userService.getName() = " + userService.getName());
+        System.out.println();
 
-            // case: local toString()
-            System.out.println("case 5: ==> [测试 local toString() 方法]");
-            System.out.println("userService.toString() = " + userService.toString());
-            System.out.println();
+        // case: override
+        System.out.println("case 4: ==> [测试重载方法返回字符串]");
+        System.out.println("userService.getName(123) = " + userService.getName(123));
+        System.out.println();
 
-            // case: 输入基本类型
-            System.out.println("case 6: ==> [常规 int 类型，返回 User 对象]");
-            System.out.println(userService.getId(10));
-            System.out.println();
+        // case: local toString()
+        System.out.println("case 5: ==> [测试 local toString() 方法]");
+        System.out.println("userService.toString() = " + userService.toString());
+        System.out.println();
 
-            System.out.println("case 7: ==> [测试参数是 long+float 类型]");
-            System.out.println("userService.getId(100L) = " + userService.getId(10L));
-            System.out.println("userService.getId(10f) = " + userService.getId(10f));
-            System.out.println();
+        // case: 输入基本类型
+        System.out.println("case 6: ==> [常规 int 类型，返回 User 对象]");
+        System.out.println(userService.getId(10));
+        System.out.println();
 
-            // case: 输入对象（User: fastjson 会将其转换为 LinkedHashMap）
-            System.out.println("case 8: ==> [测试参数是 User 类型]");
-            System.out.println("userService.getId(new User(100, \"Kai\") = " + userService.getId(new User(100, "Kai")));
-            System.out.println();
+        System.out.println("case 7: ==> [测试参数是 long+float 类型]");
+        System.out.println("userService.getId(100L) = " + userService.getId(10L));
+        System.out.println("userService.getId(10f) = " + userService.getId(10f));
+        System.out.println();
 
-            // case: 返回数组
-            System.out.println("case 9: ==> [测试返回int[]+long[]]");
-            System.out.println("Arrays.toString(userService.getIds() = " + Arrays.toString(userService.getIds()));
-            System.out.println("Arrays.toString(userService.getLongIds()) = " + Arrays.toString(userService.getLongIds()));
-            System.out.println();
+        // case: 输入对象（User: fastjson 会将其转换为 LinkedHashMap）
+        System.out.println("case 8: ==> [测试参数是 User 类型]");
+        System.out.println("userService.getId(new User(100, \"Kai\") = " + userService.getId(new User(100, "Kai")));
+        System.out.println();
 
-            // case: 输入数组
-            System.out.println("case 10: ==> [测试参数 int[], 返回 long[]]");
-            System.out.println("Arrays.toString(userService.getIds(new int[]{4, 5, 6})) = " +
-                    Arrays.toString(userService.getIds(new int[]{4, 5, 6})));
-            System.out.println();
+        // case: 返回数组
+        System.out.println("case 9: ==> [测试返回int[]+long[]]");
+        System.out.println("Arrays.toString(userService.getIds() = " + Arrays.toString(userService.getIds()));
+        System.out.println("Arrays.toString(userService.getLongIds()) = " + Arrays.toString(userService.getLongIds()));
+        System.out.println();
 
-            // case: 输入 List、输出 List
-            System.out.println("case 11: ==> [测试参数和返回值都是 List 类型]");
-            System.out.println(userService.getList(List.of(
-                    new User(100, "Kai100"),
-                    new User(101, "Kai101")
-            )));
-            System.out.println();
+        // case: 输入数组
+        System.out.println("case 10: ==> [测试参数 int[], 返回 long[]]");
+        System.out.println("Arrays.toString(userService.getIds(new int[]{4, 5, 6})) = " +
+                Arrays.toString(userService.getIds(new int[]{4, 5, 6})));
+        System.out.println();
 
-            // case: 输入 Map、输出 Map
-            System.out.println("case 12: ==> [测试参数和返回值都是 Map 类型]");
-            Map<String, User> map = new HashMap<>();
-            map.put("M200", new User(200, "Kai200"));
-            map.put("M201", new User(201, "Kai201"));
-            System.out.println(userService.getMap(map));
-            System.out.println();
+        // case: 输入 List、输出 List
+        System.out.println("case 11: ==> [测试参数和返回值都是 List 类型]");
+        System.out.println(userService.getList(List.of(
+                new User(100, "Kai100"),
+                new User(101, "Kai101")
+        )));
+        System.out.println();
 
-            System.out.println("case 13: ==> [测试参数和返回值都是 Boolean/boolean 类型]");
-            System.out.println(userService.getFlag(false));
-            System.out.println();
+        // case: 输入 Map、输出 Map
+        System.out.println("case 12: ==> [测试参数和返回值都是 Map 类型]");
+        Map<String, User> map = new HashMap<>();
+        map.put("M200", new User(200, "Kai200"));
+        map.put("M201", new User(201, "Kai201"));
+        System.out.println(userService.getMap(map));
+        System.out.println();
+
+        System.out.println("case 13: ==> [测试参数和返回值都是 Boolean/boolean 类型]");
+        System.out.println(userService.getFlag(false));
+        System.out.println();
 
 
-            // 测试 @KaiConsumer
+        // 测试 @KaiConsumer
 //            userAppService.test();
-        };
     }
 }
