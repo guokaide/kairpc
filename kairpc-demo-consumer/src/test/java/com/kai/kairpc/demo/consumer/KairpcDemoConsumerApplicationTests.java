2df868d85d2c7a42e928ca744da8b9a7b1a803c4
guokaide <kaideguo@gmail.com>
2024-03-26 07:09:10 +0800

    update consumer application test case

--- kairpc-demo-consumer/src/test/java/com/kai/kairpc/demo/consumer/KairpcDemoConsumerApplicationTests.java
+++ kairpc-demo-consumer/src/test/java/com/kai/kairpc/demo/consumer/KairpcDemoConsumerApplicationTests.java
@@ -1,13 +1,31 @@
 package com.kai.kairpc.demo.consumer;
 
+import com.kai.kairpc.demo.provider.KairpcDemoProviderApplication;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class KairpcDemoConsumerApplicationTests {
 
-    @Test
-    void contextLoads() {
+    static ApplicationContext applicationContext;
+
+    @BeforeAll
+    static void init() {
+        applicationContext = SpringApplication.run(KairpcDemoProviderApplication.class,
+                "--server.port=8084", "--logging.level.com.kai.kairpc=debug");
     }
 
+    @Test
+    void contextLoads() {
+        System.out.println("consumer test start...");
+    }
+
+    @AfterAll
+    static void destroy() {
+        SpringApplication.exit(applicationContext);
+    }
 }
