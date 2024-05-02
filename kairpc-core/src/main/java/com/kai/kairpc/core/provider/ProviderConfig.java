be5588cb7caaba7feba464ea74e1739b17302a31
guokaide <kaideguo@gmail.com>
2024-05-02 23:55:29 +0800

    add spring boot transport

--- kairpc-core/src/main/java/com/kai/kairpc/core/provider/ProviderConfig.java
+++ kairpc-core/src/main/java/com/kai/kairpc/core/provider/ProviderConfig.java
@@ -2,15 +2,18 @@ package com.kai.kairpc.core.provider;
 
 import com.kai.kairpc.core.api.RegistryCenter;
 import com.kai.kairpc.core.registry.zk.ZkRegistryCenter;
+import com.kai.kairpc.core.transport.SpringBootTransport;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.Order;
 
 @Slf4j
 @Configuration
+@Import({SpringBootTransport.class})
 public class ProviderConfig {
 
     @Bean
