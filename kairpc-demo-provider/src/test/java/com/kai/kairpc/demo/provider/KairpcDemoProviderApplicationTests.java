bffa9177409d1a67a07aca0bbe125bb9c50bb978
guokaide <kaideguo@gmail.com>
2024-07-31 23:37:55 +0800

    add apollo mockserver for provider

--- kairpc-demo-provider/src/test/java/com/kai/kairpc/demo/provider/KairpcDemoProviderApplicationTests.java
+++ kairpc-demo-provider/src/test/java/com/kai/kairpc/demo/provider/KairpcDemoProviderApplicationTests.java
@@ -1,30 +1,56 @@
 package com.kai.kairpc.demo.provider;
 
+import com.ctrip.framework.apollo.core.ApolloClientSystemConsts;
+import com.ctrip.framework.apollo.mockserver.ApolloTestingServer;
+import com.ctrip.framework.apollo.mockserver.MockApolloExtension;
+import com.kai.kairpc.core.config.ProviderProperties;
 import com.kai.kairpc.core.test.TestZkServer;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ExtendWith(MockApolloExtension.class)
 class KairpcDemoProviderApplicationTests {
 
     static TestZkServer zkServer = new TestZkServer();
+    static ApolloTestingServer apolloServer = new ApolloTestingServer();
 
+    @Autowired
+    ProviderProperties providerProperties;
+
+    @SneakyThrows
     @BeforeAll
     static void init() {
+        System.out.println("=== zk2182 ===");
         zkServer.start();
+        System.out.println("=== apollo8080 ===");
+        apolloServer.start();
     }
 
     @Test
     void contextLoads() {
         System.out.println("provider test start ...");
+        System.out.println("ApolloClientSystemConsts.APOLLO_CONFIG_SERVICE: " +
+                System.getProperty(ApolloClientSystemConsts.APOLLO_CONFIG_SERVICE));
+        System.out.println("ApolloClientSystemConsts.APOLLO_META: " +
+                System.getProperty(ApolloClientSystemConsts.APOLLO_META));
+    }
+
+    @Test
+    void printProviderProperties() {
+        System.out.println("providerProperties: " + providerProperties);
     }
 
 
     @AfterAll
     static void destroy() {
         zkServer.stop();
+        apolloServer.close();
     }
 
 }
